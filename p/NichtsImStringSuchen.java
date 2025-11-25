625437809ff8d1c3d17577ee939ca61425d12b7a
zolga-debug <bilibinskaya@gmail.com>
2025-11-25 17:50:50 +0100

    Delete p directory

--- p/NichtsImStringSuchen.java
+++
@@ -1,9 +0,0 @@
-package p;
-
-public class NichtsImStringSuchen {
-
-	public static void main(String[] args) {
-		System.out.println("Hallo".contains("")); // true
-	}
-
-}
--- p/g/TopDown/AndereMain.java
+++
@@ -1,9 +0,0 @@
-package p.g.TopDown;
-
-public class AndereMain {
-
-	public static void main(String[] args) {
-		MenueMitAction.nichtInstalliertMeldung();
-	}
-
-}
